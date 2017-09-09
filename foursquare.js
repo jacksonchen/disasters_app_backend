2aa0202dfb71881eca35e6a93d3f93c909ceaf84
Jackson Chen <jackson.chen@columbia.edu>
2017-09-08 23:02:24 -0400

    Fix foursquare

--- foursquare.js
+++ foursquare.js
@@ -3,21 +3,21 @@ var request = require('request'),
     client_id = config.get('Foursquare.CLIENT_ID'),
     client_secret = config.get('Foursquare.CLIENT_SECRET');
 
-foursquare = function(lat, long) {
-  // Philly lat long 39.9,-75.2
+foursquareAPI = function(lat, long, callback) {
   var base = 'https://api.foursquare.com/v2/venues/search?ll=';
   base = base.concat(lat, ',', long);
   base = base.concat('&categoryId=4bf58dd8d48988d1f9941735&client_id=');
   base = base.concat(client_id, '&client_secret=', client_secret, '&v=20170908');
-  console.log(base);
   request(base, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-          return body;
+        callback(body);
       }
-      return null;
+      callback(null);
   });
 }
 
-// var foursquare = module.exports.foursquare;
+var foursquare = module.exports.foursquareAPI;
 
-console.log(foursquare(39.9, 75.2));
+// foursquare(39.9,-75.2, function(ans) {
+//   console.log(ans);
+// });
